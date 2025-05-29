b0e2d9969ecfcbf3dd1bb71076c8d6bfaf5ed200
JEUN BYEONGIN <alvin1115@naver.com>
2025-05-29 16:17:56 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+struct node {
+	char left;
+	char right;
+};
+
+vector<node> v(1000);
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	priority_queue<int> pq;
+	int N; cin >> N;
+	while (N--) {
+		int a;
+		cin >> a;
+
+		if (a == 0) {
+			if (pq.empty()) cout << 0 << "\n";
+			else {
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else {
+			pq.push(a);
+		}
+	}
+	return 0;
+}
