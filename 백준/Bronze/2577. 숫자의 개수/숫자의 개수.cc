adbbf42e7443cad59d0cddf29b00a7133257b94e
JEUN BYEONGIN <alvin1115@naver.com>
2025-05-29 16:22:45 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int person_Num;
+    int sum = 1;
+    int num[10] = {0, };
+    for (int a = 0; a < 3; a++) {
+        cin >> person_Num;
+        sum *= person_Num;
+    }
+    while (sum > 0) {
+        num[sum % 10]++;
+        sum /= 10;
+   }
+    for (int a = 0; a < 10;  a++) {
+        cout << num[a] << endl;
+    }
+}
