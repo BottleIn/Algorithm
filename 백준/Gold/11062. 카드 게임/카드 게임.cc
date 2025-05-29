3d525bce95028a6202ea57b3e55d0307b6594dcf
JEUN BYEONGIN <alvin1115@naver.com>
2025-05-29 16:05:11 +0900

    [Gold III] Title: 카드 게임, Time: 120 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/11062. 카드 게임/카드 게임.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int T, N, arr[1001];
+int dp[1001][1001]; //i번째부터 j번째 카드가 있을 때 근우가 얻을 수 있는 최대 점수
+
+int CardGame(int left, int right, int turn) {
+	if (left > right) return 0;
+	if (dp[left][right]) return dp[left][right];
+
+	if (turn % 2) // 근우 차례
+		return dp[left][right] = max(arr[left] + CardGame(left + 1, right, turn + 1), arr[right] + CardGame(left, right - 1, turn + 1));
+	
+	else // 명우 차례 
+		return dp[left][right] = min(CardGame(left + 1, right, turn + 1), CardGame(left, right - 1, turn + 1));
+}
+
+int main() {
+	cin >> T;
+	for (int t = 0; t < T; t++) {
+		cin >> N;
+		for (int n = 0; n < N; n++) cin >> arr[n];
+		CardGame(0, N - 1, 1);
+		
+		
+		cout << dp[0][N - 1] << "\n";
+		memset(dp, 0, sizeof(dp));
+	}
+	return 0;
+}
