881d774d022757589c510aceb0599fdb25a7d121
JEUN BYEONGIN <alvin1115@naver.com>
2026-01-13 14:58:47 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    
+    odds = ''
+    evens = ''
+    
+    for num in num_list:
+        if num % 2 == 0:
+            evens += str(num)
+        else:
+            odds += str(num)
+            
+    answer = int(odds) + int(evens)
+    return answer
