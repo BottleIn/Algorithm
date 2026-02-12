511043196e2a52cc10b02d58c590876b6a2d94c4
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-12 18:55:52 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.44 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,11 +1,16 @@
-import math
-
 def solution(left, right):
-    ans = 0
-    for i in range(left, right + 1):
-        x = math.isqrt(i)
-        if x * x == i:   # 완전제곱수
-            ans -= i
+    answer = 0
+    for num in range(left,right+1):
+        tmp = []
+        for x in range(1, int(num ** (1/2) + 1)):
+            if num % x == 0:
+                tmp.append(x)
+                if (x != num//x):
+                    tmp.append(num//x)
+        #print(tmp)
+        n = len(tmp)
+        if n % 2 == 0:
+            answer +=  num
         else:
-            ans += i
-    return ans
+            answer -= num
+    return answer
