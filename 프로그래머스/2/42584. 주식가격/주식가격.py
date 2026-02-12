738c90d1c4c4ee081a213598bdee09816c9fad91
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-12 18:15:55 +0900

    [level 2] Title: 주식가격, Time: 56.27 ms, Memory: 25 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,19 +1,29 @@
+from collections import deque
 def solution(prices):
+    
     n = len(prices)
-    answer = [0] * n
-    stack = []
-    
-    for idx in range(n):
+    answer = [0 for _ in range(n)]
+    d = deque()
+    #prices = deque(prices)
+    t = deque()
+    for idx, x in enumerate(prices):
+        t.append((x,idx))
+    #print(t)
+    while t:
+        x,idx = t.popleft()
+        if not d:
+            d.append((x,idx))
+        else:
+            if x >= d[-1][0]:
+                d.append((x,idx))
+            else:
+                while d and x < d[-1][0]:
+                    x_t,idx_t = d.pop()
+                    answer[idx_t] = idx - idx_t
+                d.append((x,idx))
+    #print(d)
+    for x,idx in d:
         
-        while stack and prices[idx] < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = idx - j
-        stack.append(idx)
-    
-    while stack:
-        i = stack.pop()
-        answer[i] = n - 1 - i
-    
-    
-    
+        answer[idx] = (n-1) - idx
+    #print(answer)
     return answer
