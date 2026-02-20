3ce5b00293c997ff8b069db4a610c049fc7f96ec
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-20 13:32:52 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,13 +1,8 @@
 def solution(nums):
-    max_num = int(len(nums) / 2)
-    print(max_num)
+    answer = 0
+    n = len(nums) // 2
+    a = set(nums)
+    b = len(a)
     
-    a=set(nums)
-    print(a)
     
-    if len(a)<max_num :
-        return len(a)
-    else:
-        return max_num
-    
-    return answer
+    return min(b,n)
