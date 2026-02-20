5714ddca84e64b13a22d3ba58c2a0167eb958ad8
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-20 14:15:54 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 66.80 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,16 +1,9 @@
-from collections import deque
-
 def solution(arr):
-    is_dup = deque()
-    for x in arr:
-        if len(is_dup) == 0:
-            is_dup.append(x)
-        else:
-            num = is_dup.pop()
-            if(num == x):
-                is_dup.append(x)
-            else:
-                is_dup.append(num)
-                is_dup.append(x)
-    ans = list(is_dup)
-    return ans
+    answer = []
+    
+    for n in arr:
+        if not answer:
+            answer.append(n)
+        elif answer[-1] != n:
+            answer.append(n)
+    return answer
