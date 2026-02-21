5d5b8b09d1ed2b8b331e59c67498c69d778dee1e
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-21 16:32:57 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,37 +1,13 @@
-from collections import deque
+def solution(n, a, b):
+    answer = 0
 
-def is_valid(x,y):
-    if abs(x-y) != 1:
-        return False
-    a = min(x,y)
-    b = max(x,y)
-    if a % 2 != 0 and b % 2 == 0:
-        return True
-    return False
-
-
-
-def solution(n,a,b):
-    answer = 1
-    d = deque([a,b])
-    # is_cor = is_valid(d[0],d[1])
-    # print(is_cor)
-    
-    # print(is_valid(d[0],d[1]))
-    # print(abs(d[0]-d[1]))
-    
-    while not is_valid(d[0],d[1]) or abs(d[0]-d[1]) != 1:
-        for x in list(d): 
-            if x % 2 == 0:
-                d.append(x // 2)
-            else:
-                d.append((x + 1) // 2)
-        for _ in range(2):
-            d.popleft()
+    while a != b:
+        # 1. 다음 라운드 번호로 갱신
+        # (a + 1) // 2 와 동일한 논리
+        a = (a // 2) + (a % 2)
+        b = (b // 2) + (b % 2)
+        
+        
         answer += 1
+
     return answer
-
-
-# print(is_valid(2,3))
-# print(is_valid(1,2))
-# print(solution(8,2,3))
