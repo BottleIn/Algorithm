c5c30ccca4202238a391e699fecc1f684e91bcfa
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-21 16:57:32 +0900

    [level 2] Title: 할인 행사, Time: 826.89 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,18 +1,32 @@
+from collections import defaultdict, Counter
+
 def solution(want, number, discount):
     answer = 0
-    target = {w: n for w, n in zip(want, number)}
+    want_dict = {}
+    for w, n in zip(want, number):
+        want_dict[w] = n
     
+    want_counter = Counter(want_dict)
     
-    for i in range(len(discount) - 9):
-        
-        current_10_days = discount[i : i + 10]
-        
-        current_dic = {}
-        for item in current_10_days:
-            if item in target:
-                current_dic[item] = current_dic.get(item, 0) + 1
-        
-        # 5. 목표(target)와 현재(current_dic)가 같은지 비교
-        if current_dic == target:
+    cur_dict = defaultdict(int)
+    for x in range(10):
+        cur_dict[discount[x]] += 1
+    
+    l = 0
+    r = 10
+    
+    # len(discount)까지 가야 마지막 10일째 구간을 검사할 수 있습니다.
+    while r <= len(discount):
+        # [수정 포인트 1] cur_dict를 Counter로 변환 후 빈 Counter를 더해 0인 항목 제거
+        if want_counter == (Counter(cur_dict) + Counter()):
             answer += 1
+        
+        # [수정 포인트 2] r이 범위 내에 있을 때만 갱신
+        if r < len(discount):
+            cur_dict[discount[l]] -= 1
+            cur_dict[discount[r]] += 1
+        
+        l += 1
+        r += 1
+    
     return answer
