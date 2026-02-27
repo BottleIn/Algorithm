fdaf071221b086f2dfb039fdcd7aa1b85babd1df
JEUN BYEONGIN <alvin1115@naver.com>
2026-02-27 16:16:44 +0900

    [level 2] Title: 더 맵게, Time: 1814.61 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,16 @@
 import heapq
-
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
-    
+    num = 0
     
     while scoville[0] < K:
         if len(scoville) < 2:
             return -1
-            
         answer += 1
-        a1 = heapq.heappop(scoville)
-        a2 = heapq.heappop(scoville)
-        cur = a1 + (a2 * 2)
-        heapq.heappush(scoville, cur)
-        
+        num1 = heapq.heappop(scoville)
+        num2 = heapq.heappop(scoville)
+        num = num1 + (2 * num2)
+        heapq.heappush(scoville,num)
+        #print(num1,num2, scoville)
     return answer
