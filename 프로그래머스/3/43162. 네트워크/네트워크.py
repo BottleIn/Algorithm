859175eaf7c8b49529a59fc781a360683d67b13b
JEUN BYEONGIN <alvin1115@naver.com>
2026-03-04 18:22:52 +0900

    [level 3] Title: 네트워크, Time: 1.71 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,32 +1,38 @@
 from collections import defaultdict,deque
-
-
 def solution(n, computers):
     answer = 0
-    visited = [False] * n
-    # print(visited)
-    q = deque()
+    n,m = len(computers), len(computers[0])
+    d = defaultdict(list)
+    
+    
+    for x in range(n):
+        for y in range(m):
+            if x == y:
+                continue
+            if computers[x][y] == 1:
+                d[x].append(y)
+    visited = [False for _ in range(n)]
+    
+    #print(visited)
+    
     def bfs(start):
-
-        nonlocal visited,answer
-        if visited[start] == True:
+        nonlocal answer
+        if visited[start]:
             return
         
-        q.append(start)
-
-        while q:
-            cur = q.popleft()
-            visited[cur] = True
-            for idx, val in enumerate(computers[cur]):
-                if visited[idx] == False:
-                    if idx != start and val == 1:
-                        #print(idx, start)
-                        q.append(idx)
-                        #print(q)
+        dq = deque([start])
+        
+        while dq:
+            node = dq.popleft()
+            visited[node] = True
+            
+            for neigh in d[node]:
+                if not visited[neigh]:
+                    dq.append(neigh)
         answer += 1
-                    
+    
+    
     for x in range(n):
         bfs(x)
+    
     return answer
-
-# print(solution(3,[[1, 1, 0], [1, 1, 1], [0, 1, 1]]))
