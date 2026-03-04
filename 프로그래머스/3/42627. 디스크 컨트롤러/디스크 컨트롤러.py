3e5ddaff3bd13a5b776b942734e7dcd335cac6fe
JEUN BYEONGIN <alvin1115@naver.com>
2026-03-04 15:12:36 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 9.68 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,34 +1,40 @@
 '''
-작업의 소요시간(l) 작은거 > 작업의 요청 시각(s) 빠른거 >작업의 번호가 작은 것 (idx)
+배출 우선순위 : 소요시간(l), 요청 시각(s), 작업 번호(idx)
 '''
 import heapq
-
 def solution(jobs):
-    #요청 시간 순으로 정렬
-    jobs.sort()
+    answer = 0
+    jobs_order = []
     
-    count = 0      # 처리된 작업 개수
-    last_time = -1 # 마지막 작업이 시작된 시간
-    now = 0        # 현재 시간
-    total_time = 0 # 각 작업의 (종료시간 - 요청시간) 합계
-    wait_list = [] # 대기 중인 작업들 (최소 힙)
-
-    while count < len(jobs):
-        for job in jobs:
-            # 마지막 작업 시작 이후 ~ 현재 시각 사이에 요청된 작업을 힙에 추가
-            if last_time < job[0] <= now:
-                # (소요시간, 요청시간)
-                heapq.heappush(wait_list, [job[1], job[0]])
+    for idx, (s, l) in enumerate(jobs):
+        jobs_order.append((s,l,idx))
+    
+    jobs_order.sort()  # 요청 시각 순서에 따라 정렬
+    n = len(jobs_order) # 요청 일 개수
+    
+    now = 0 # 현재 시각
+    last_end_time = -1 # 이전 일이 시작한 시간
+    count = 0 # 처리한 jobs 개수  -> n개 될 때까지 
+    total = 0 # 전체 처리한 시간
+    d = [] # 대기 큐
+    
+    #시간을 증가시켜가며 비교
+    while count < n:            #요청된 일을 다 처리할 때 까지
+        for jobs in jobs_order: # 처리할 수 있는 일들인지 확인
+            if last_end_time < jobs[0] <= now:
+                heapq.heappush(d, [jobs[1],jobs[0],jobs[2]])        
         
-        if wait_list:
-            # 3. 대기열 중 소요 시간이 가장 짧은 작업 꺼내기
-            duration, request_time = heapq.heappop(wait_list)
-            last_time = now
+        
+        if d:  #제일 짧은 것만 나오게
+            duration, request_time, idx = heapq.heappop(d)
+            last_end_time = now
             now += duration
-            total_time += (now - request_time) # 종료시간 - 요청시간
+            total += (now - request_time)
             count += 1
+        
         else:
-            # 4. 당장 처리할 작업이 없다면 다음 작업 요청 시간으로 이동
             now += 1
-            
-    return total_time // len(jobs)
+        
+    
+    
+    return total // n
