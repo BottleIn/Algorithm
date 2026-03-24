1a07252f0ed868e6f55c16bbba230390a97a2401
JEUN BYEONGIN <alvin1115@naver.com>
2026-03-24 15:55:39 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -3,21 +3,18 @@ from collections import deque
 def main():
     input = sys.stdin.readline
 
+    # 1) N, K
     N, K = map(int, input().split())
-    # 입력만 받는 코드 (출력 없음)
-    dp = deque()
-    for x in range(1,N+1):
-        dp.append(x)
+    dq = deque(range(1, N + 1))
+    #print(nums)
     ans = []
-    tp = 0
-    while dp:
-        tp += 1
-        if tp == K:
-            ans.append(dp.popleft())
-            tp = 0
-            continue
-        dp.append(dp.popleft())
+    while dq:
+        dq.rotate(-K)
+        #print(dq)
+        ans.append(dq.pop())
+    
     result = f"<{', '.join(map(str, ans))}>"
     print(result)
+
 if __name__ == "__main__":
     main()
